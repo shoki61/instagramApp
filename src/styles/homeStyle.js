25ba7e40fcec8c15f9a2b30d8d6d0b4d260a0ffa
Sohrat <sohrat6128@gmail.com>
2020-01-28 18:25:28 +0300

    home page sections added

--- src/styles/homeStyle.js
+++ src/styles/homeStyle.js
@@ -8,15 +8,51 @@ export default StyleSheet.create({
     container:{
         width:w,
         minHeight:h,
-        backgroundColor:'black'
+        backgroundColor:'gold'
     },
     header:{
-        height:h/9,
+        height:h/11,
         backgroundColor:'#1c1c1c',
         justifyContent:'space-between',
         flexDirection:'row',
         padding:15
     },
+    storiesPart:{
+        height:h/5.5,
+        maxHeight:h/5.5,
+        flexDirection:'row',
+        backgroundColor:'black',
+        paddingLeft:15,
+        alignItems:'center'
+    },
+    userContainer:{
+        alignItems:'center',
+        flexDirection:'column',
+        paddingLeft:10,
+        paddingRight:10,
+        margin:5,
+    },
+    storyProfileName:{
+        fontSize:17,
+        color:'white',
+    },
+    userInfoSection:{
+        backgroundColor:'grey',
+        justifyContent:'space-between',
+        alignItems:'center',
+        flexDirection:'row',
+        padding:15,
+        height:h/9.5,
+        width:'100%',            
+    },
+    userName:{
+        color:'white',
+        fontWeight:'bold',
+        fontSize:20,
+        paddingLeft:14
+    }
+
+
 
 
 });
--- src/views/home.js
+++ src/views/home.js
@@ -10,14 +10,45 @@ class Home extends React.Component{
             <View style={styles.container}>
                 <View style={styles.header}>
                     <TouchableOpacity>
-                        <SImage width={43} source={require('../images/cameraIcon.png')}/>
+                        <SImage width={30} source={require('../images/cameraIcon.png')}/>
                     </TouchableOpacity>
 
-                    <SImage width={180} source={require('../images/instagramText.png')}/>
+                    <SImage width={143} source={require('../images/instagramText.png')}/>
                     
                     <TouchableOpacity>
-                        <SImage width={43} source={require('../images/messageIcon.png')}/>
+                        <SImage width={30} source={require('../images/messageIcon.png')}/>
                     </TouchableOpacity>
+                </View>
+
+                <View style={styles.storiesPart}>
+                  <View style={styles.userContainer}>
+                    <SImage height={80} width={80} borderRadius={100} source={require('../images/profilFoto.jpg')}/>
+                    <Text style={styles.storyProfileName} >Bruce Wayne</Text>
+                  </View>
+                  <View style={styles.userContainer}>
+                    <SImage height={80} width={80} borderRadius={100} source={require('../images/profilFoto.jpg')}/>
+                    <Text style={styles.storyProfileName} >Bruce Wayne</Text>
+                  </View> 
+                  <View style={styles.userContainer}>
+                    <SImage height={80} width={80} borderRadius={100} source={require('../images/profilFoto.jpg')}/>
+                    <Text style={styles.storyProfileName} >Bruce Wayne</Text>
+                  </View>       
+                </View>
+
+                <View style={styles.posts}>
+                    <View style={styles.userPostSection}>
+                        <View style={styles.userInfoSection}>
+
+                            <View style={{flexDirection:'row',alignItems:'center'}}>
+                               <SImage height={45} width={45} borderRadius={100} source={require('../images/profilFoto.jpg')}/>
+                               <Text style={styles.userName}>Spiderman</Text>
+                            </View>
+                            <SImage height={20} width={20} source={require('../images/threeDot.png')} />
+
+                        </View>
+                        <View style={styles.postSection}></View>
+                    </View>
+
                 </View>
 
 
